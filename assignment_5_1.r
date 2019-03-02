4832bdc051f71ee9a45888cd410fb7779960f2a1
Achu-nair <42400273+Achu-nair@users.noreply.github.com>
2019-03-02 15:08:13 +0530

    Add files via upload

---
+++ assignment_5_1.r
@@ -0,0 +1,31 @@
+#ASSIGNMENT 5.1
+library(stringr)
+
+#calculate the number of vovwels in USA STATES.
+
+#names of states
+states = row.names(USArrests)
+states
+#vector vowels
+vowels = c("a","e","i","o","u")
+vowels
+
+#store the results
+
+numv = vector(mode = "integer",length = 5)
+
+#calculate the number of vowels
+for(j in seq_along(vowels))
+{
+  numc = str_count(tolower(states),vowels[j])
+  numv[j] = sum(numc)
+}
+names(numv) = vowels
+numv
+
+sort(numv, decreasing = TRUE)
+
+barplot(numv, main = "Number of vowels in USA States names",xlab = "vowels",
+        ylab = "number",border = NA, ylim = c(0, 80),col = "orange")
+
+
